c98bd7f1217672575e4a833e09ed4d4e4b914650
Noookia <matchman7980@gmail.com>
2023-12-01 18:42:01 +0800

    fixing fov cone

--- Assets/Scripts/FOVCone.cs
+++ Assets/Scripts/FOVCone.cs
@@ -24,7 +24,7 @@ public class VisionCone : MonoBehaviour
         VisionAngle = 90f;
         VisionAngle *= Mathf.Deg2Rad;
         VisionRange = 15f;
-        VisionConeResolution = 20;
+        VisionConeResolution = 10;
     }
 
     void Update()
@@ -70,42 +70,42 @@ public class VisionCone : MonoBehaviour
 
     void DrawVisionCone()//this method creates the vision cone mesh
     {
-	int[] triangles = new int[(VisionConeResolution - 1) * 3];
-    	Vector3[] Vertices = new Vector3[VisionConeResolution + 1];
-        Vertices[0] = Vector3.zero;
-        float Currentangle = -VisionAngle / 2;
-        float angleIcrement = VisionAngle / (VisionConeResolution - 1);
-        float Sine;
-        float Cosine;
+        int[] triangles = new int[(VisionConeResolution - 1) * 3];
+            Vector3[] Vertices = new Vector3[VisionConeResolution + 1];
+            Vertices[0] = Vector3.zero;
+            float Currentangle = -VisionAngle / 2;
+            float angleIcrement = VisionAngle / (VisionConeResolution - 1);
+            float Sine;
+            float Cosine;
 
-        for (int i = 0; i < VisionConeResolution; i++)
-        {
-            Sine = Mathf.Sin(Currentangle);
-            Cosine = Mathf.Cos(Currentangle);
-            Vector3 RaycastDirection = (transform.forward * Cosine) + (transform.right * Sine);
-            Vector3 VertForward = (Vector3.forward * Cosine) + (Vector3.right * Sine);
-            if (Physics.Raycast(transform.position, RaycastDirection, out RaycastHit hit, VisionRange, obstacleMask))
+            for (int i = 0; i < VisionConeResolution; i++)
             {
-                Vertices[i + 1] = VertForward * hit.distance;
-                Debug.Log("in sight");
+                Sine = Mathf.Sin(Currentangle);
+                Cosine = Mathf.Cos(Currentangle);
+                Vector3 RaycastDirection = (transform.forward * Cosine) + (transform.right * Sine);
+                Vector3 VertForward = (Vector3.forward * Cosine) + (Vector3.right * Sine);
+                if (Physics.Raycast(transform.position, RaycastDirection, out RaycastHit hit, VisionRange, obstacleMask))
+                {
+                    Vertices[i + 1] = VertForward.normalized * hit.distance;
+                    Debug.Log(hit.distance);
+                }
+                else
+                {
+                    Vertices[i + 1] = VertForward.normalized * VisionRange/3;
+                }
+
+
+                Currentangle += angleIcrement;
             }
-            else
+            for (int i = 0, j = 0; i < triangles.Length; i += 3, j++)
             {
-                Vertices[i + 1] = VertForward * VisionRange;
+                triangles[i] = 0;
+                triangles[i + 1] = j + 1;
+                triangles[i + 2] = j + 2;
             }
-
-
-            Currentangle += angleIcrement;
-        }
-        for (int i = 0, j = 0; i < triangles.Length; i += 3, j++)
-        {
-            triangles[i] = 0;
-            triangles[i + 1] = j + 1;
-            triangles[i + 2] = j + 2;
-        }
-        VisionConeMesh.Clear();
-        VisionConeMesh.vertices = Vertices;
-        VisionConeMesh.triangles = triangles;
-        MeshFilter_.mesh = VisionConeMesh;
+            VisionConeMesh.Clear();
+            VisionConeMesh.vertices = Vertices;
+            VisionConeMesh.triangles = triangles;
+            MeshFilter_.mesh = VisionConeMesh;
     }
 }
