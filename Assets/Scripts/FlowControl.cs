226af52eefa3096892d891352e22728cd1008c71
威猛 黃 <8334551will@gmail.com>
2023-12-03 21:49:22 +0800

    addRigidBody

--- Assets/Scripts/FlowControl.cs
+++ Assets/Scripts/FlowControl.cs
@@ -6,24 +6,30 @@ public class FlowControl : MonoBehaviour
 {
     [SerializeField] public GameObject Goodguy_Prefab;
     [SerializeField] public GameObject Badguy_Prefab;
-    [SerializeField] public Vector3 spawnPosition; // 
+    [SerializeField] public Vector3 spawnCenter = new Vector3(0f, 5.0f, 0f);
+    [SerializeField] public float spawnCircleRad = 5.0f;
     [SerializeField] public int numGoodguy = 10;
-
+    private List<GameObject> goodGuysList = new List<GameObject>();
     void Start()
     {
-        spawnPosition = new Vector3(0f, 5.0f, 0f);
         Generate_Creatures();
     }
-    // void Update()
-    // {
-    // }
+    void Update()
+    {
+
+    }
     void Generate_Creatures()
     {
         if (Goodguy_Prefab != null)
         {
-            for (int i=0; i<numGoodguy; i++) // Can try generate around the alert table
+            for (int i=0; i<numGoodguy; i++)
             {
-                GameObject goodGuy = Instantiate(Goodguy_Prefab, spawnPosition, Quaternion.identity);
+                float angle = i * (360f / numGoodguy); // Calculate the angle of each spawning point on the circle
+                float x = spawnCenter.x + spawnCircleRad * Mathf.Cos(Mathf.Deg2Rad * angle);
+                float z = spawnCenter.z + spawnCircleRad * Mathf.Sin(Mathf.Deg2Rad * angle);
+                Vector3 spawnPos = new Vector3(x, spawnCenter.y, z);
+                GameObject goodGuy = Instantiate(Goodguy_Prefab, spawnPos, Quaternion.identity);
+                goodGuysList.Add(goodGuy);
             }
         }
         else
