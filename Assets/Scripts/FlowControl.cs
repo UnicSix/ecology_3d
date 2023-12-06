08b85e2c08752b26918842d1658d62562aa4415d
威猛 黃 <8334551will@gmail.com>
2023-12-06 14:58:38 +0800

    GuysCircleGeneration

--- Assets/Scripts/FlowControl.cs
+++ Assets/Scripts/FlowControl.cs
@@ -9,7 +9,8 @@ public class FlowControl : MonoBehaviour
     [SerializeField] public Vector3 spawnCenter = new Vector3(0f, 5.0f, 0f);
     [SerializeField] public float spawnCircleRad = 5.0f;
     [SerializeField] public int numGoodguy = 10;
-    private List<GameObject> goodGuysList = new List<GameObject>();
+    [SerializeField] public int numBadguy = 2;
+    private List<GameObject> GuysList = new List<GameObject>();
     void Start()
     {
         Generate_Creatures();
@@ -21,23 +22,40 @@ public class FlowControl : MonoBehaviour
     
     void Generate_Creatures()
     {
-        if (Goodguy_Prefab != null)
-        {
-            for (int i=0; i<numGoodguy; i++)
+        if (Goodguy_Prefab != null && Badguy_Prefab != null) {
+            int numGenGoodguys = 0;
+            int numGenBadguys = 0;
+
+            for (int i=0; i<(numGoodguy+numBadguy); i++)
             {
-                float angle = i * (360f / numGoodguy); // Calculate the angle of each spawning point on the circle
+                float angle = i * (360f / (numGoodguy+numBadguy)); // Calculate the angle of each spawning point on the circle
                 float x = spawnCenter.x + spawnCircleRad * Mathf.Cos(Mathf.Deg2Rad * angle);
                 float z = spawnCenter.z + spawnCircleRad * Mathf.Sin(Mathf.Deg2Rad * angle);
                 Vector3 spawnPos = new Vector3(x, spawnCenter.y, z);
                 Quaternion rotation = Quaternion.LookRotation(spawnCenter - spawnPos); // Face Center
 
-                GameObject goodGuy = Instantiate(Goodguy_Prefab, spawnPos, rotation);
-                goodGuysList.Add(goodGuy);
+                GameObject creaturePrefab; // Random sequence
+                if (Random.Range(0f, 1f) < 0.5f && numGenGoodguys < numGoodguy) {
+                    creaturePrefab = Goodguy_Prefab;
+                    numGenGoodguys++;
+                }
+                else {
+                    if (numGenBadguys < numBadguy) {
+                        creaturePrefab = Badguy_Prefab;
+                        numGenBadguys++;
+                    }
+                    else {
+                        creaturePrefab = Goodguy_Prefab;
+                        numGenGoodguys++;
+                    }
+                }
+
+                GameObject guy = Instantiate(creaturePrefab, spawnPos, rotation); // Both good and bad
+                GuysList.Add(guy);
             }
         }
-        else
-        {
-            Debug.LogError("(GoodGuy PreFab is NULL)");
+        else {
+            Debug.LogError("(PreFab is NULL)");
         }
     }
 }
